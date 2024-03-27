129c4ccdf0b245703acb5569bd2b041f2e00eba6
chavdar.momchilov <chavmom@gmail.com>
2024-03-27 11:33:27 -0400

    On branch master  Your branch is up to date with 'origin/master'.  Changes to be committed: 	modified:   src/app/app.component.ts 	modified:   src/app/app.routes.ts 	modified:   src/app/home/home.component.css 	modified:   src/app/home/home.component.html 	modified:   src/styles.css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
-import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, NavbarComponent, HomeComponent, RouterOutlet],
+  imports: [CommonModule, NavbarComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
