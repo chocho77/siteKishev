fb826d31e08c9c7f049861c580327c4ef35c601e
chavdar.momchilov <chavmom@gmail.com>
2024-04-04 08:24:03 +0300

    On branch temp-branch  Your branch is up to date with 'origin/temp-branch'.  Changes to be committed: 	modified:   src/app/home/home.component.html 	modified:   src/app/home/home.component.ts 	new file:   src/assets/images/bocianie-gniazda.png 	new file:   src/assets/images/cennik.png 	new file:   src/assets/images/hamaki.png

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -8,5 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  swingsImageUrl = "assets/images/bocianie-gniazda.png";
+  consumableImageUrl = "assets/images/hamaki.png";
 
 }
