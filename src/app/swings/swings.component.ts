7516edead804bb689f6d996da248c2af7ca16b01
chocho77 <chavmom@gmail.com>
2024-04-11 13:02:14 +0300

    On branch temp-branch  Changes to be committed: 	modified:   src/app/swings/swings.component.ts

--- src/app/swings/swings.component.ts
+++ src/app/swings/swings.component.ts
@@ -10,5 +10,10 @@ import { Component } from '@angular/core';
 export class SwingsComponent {
   // titleImg :  Люлка за щъркелово гнездо 125 см - цветна (без мрежа) ГИГАНТ 
   nestswing125GiantUrl="assets/imgswings/nestswing125Giant/nestswing125Giant.jpg";
- 
+  // titleImg : Люлка за щъркелово гнездо 125 см - цветна (с мрежа) ГИГАНТ
+  storkNestSwing125GiantUrl = "assets/imgswings/storkNestSwing125Giant/storkNestSwing125Giant.jpg";
+  // titleImg : Люлка щъркелово гнездо 60 см - цветна (без мрежа)
+  storkNestSwingCoWOutNetUrl="assets/imgswings/storkNestSwing60CoWOutNet/storkNestSwingCoWOutNet.jpg";
+  //
+  
 }
