583fc18520fb2f3331ccf4b90e99fb9f47f6be83
chocho77 <chavmom@gmail.com>
2024-04-21 20:58:20 +0300

    On branch temp-branch  Your branch is up to date with 'origin/temp-branch'.  Changes to be committed: 	modified:   src/app/storknestswing125giantwithnet/storknestswing125giantwithnet.component.html 	modified:   src/app/storknestswing125giantwithnet/storknestswing125giantwithnet.component.ts 	modified:   src/app/storknestswing125giantwoutnet/storknestswing125giantwoutnet.component.html 	modified:   src/app/storknestswing125giantwoutnet/storknestswing125giantwoutnet.component.ts

--- src/app/storknestswing125giantwoutnet/storknestswing125giantwoutnet.component.ts
+++ src/app/storknestswing125giantwoutnet/storknestswing125giantwoutnet.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-storknestswing125giantwoutnet',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './storknestswing125giantwoutnet.component.html',
   styleUrl: './storknestswing125giantwoutnet.component.css'
 })
