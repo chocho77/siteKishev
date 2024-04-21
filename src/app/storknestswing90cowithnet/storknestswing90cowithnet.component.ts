6659352a007e1a9393e81e9aa462a8b6a870a21e
chocho77 <chavmom@gmail.com>
2024-04-21 07:14:46 +0300

    On branch temp-branch  Your branch is up to date with 'origin/temp-branch'.  Changes to be committed: 	modified:   src/app/storknestswing90cowithnet/storknestswing90cowithnet.component.html 	modified:   src/app/storknestswing90cowithnet/storknestswing90cowithnet.component.ts

--- src/app/storknestswing90cowithnet/storknestswing90cowithnet.component.ts
+++ src/app/storknestswing90cowithnet/storknestswing90cowithnet.component.ts
@@ -8,14 +8,12 @@ import { Component } from '@angular/core';
   styleUrl: './storknestswing90cowithnet.component.css'
 })
 export class Storknestswing90cowithnetComponent {
-    // titleImg: Люлка щъркелово гнездо 90 см -цветна (без мрежа)
-    storkNestSwing90CoWOutNetUrl = "assets/imgswings/storkNestSwing90CoWOutNet/storkNestSwing90CoWOutNet.jpg";
+   
+     // titleImg: Люлка щъркелово гнездо 90 см - цветна (с мрежа)
+     storkNestSwing90CoWithNetUrl = "assets/imgswings/storkNestSwing90CoWithNet/storkNestSwing90CoWithNet.jpg";
   
     // similar products
-    
-     // titleImg: Люлка щъркелово гнездо 90 см - цветна (с мрежа)
-    storkNestSwing90CoWithNetUrl = "assets/imgswings/storkNestSwing90CoWithNet/storkNestSwing90CoWithNet.jpg";
-  
+     
      // titleImg: Люлка щъркелово гнездо 90 см - цветна фина мрежа
    storkNestSwing90CoFineMeshUrl = "assets/imgswings/storkNestSwing90CoFineMesh/storkNestSwing90CoFineMesh.jpg";
   
@@ -24,5 +22,8 @@ export class Storknestswing90cowithnetComponent {
   
      // titleImg :  Люлка за щъркелово гнездо 125 см - цветна (без мрежа) ГИГАНТ 
      nestswing125GiantUrl="assets/imgswings/nestswing125Giant/nestswing125Giant.jpg";
+
+      // titleImg : Люлка за щъркелово гнездо 125 см - цветна (с мрежа) ГИГАНТ
+      storkNestSwing125GiantUrl = "assets/imgswings/storkNestSwing125Giant/storkNestSwing125Giant.jpg";
 
 }
