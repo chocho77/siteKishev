1930a93b808eb507313864547520afc6b6dd236d
chocho77 <chavmom@gmail.com>
2024-04-21 08:23:26 +0300

    On branch temp-branch  Your branch is up to date with 'origin/temp-branch'.  Changes to be committed: 	modified:   src/app/storknestswing90cofinemeshrainbow/storknestswing90cofinemeshrainbow.component.css 	modified:   src/app/storknestswing90cofinemeshrainbow/storknestswing90cofinemeshrainbow.component.html 	modified:   src/app/storknestswing90cofinemeshrainbow/storknestswing90cofinemeshrainbow.component.ts

--- src/app/storknestswing90cofinemeshrainbow/storknestswing90cofinemeshrainbow.component.ts
+++ src/app/storknestswing90cofinemeshrainbow/storknestswing90cofinemeshrainbow.component.ts
@@ -8,5 +8,22 @@ import { Component } from '@angular/core';
   styleUrl: './storknestswing90cofinemeshrainbow.component.css'
 })
 export class Storknestswing90cofinemeshrainbowComponent {
+    
+      // titleImg: Люлка щъркелово гнездо 90 см - цветна фина мрежа RAINBOW
+     storkNestSwing90CoFineMeshRainbowUrl = "assets/imgswings/storkNestSwing90CoFineMeshRainbow/storkNestSwing90CoFineMeshRainbow.jpg";
+
+     // similar products  
+      
+      // titleImg :  Люлка за щъркелово гнездо 125 см - цветна (без мрежа) ГИГАНТ 
+      nestswing125GiantUrl="assets/imgswings/nestswing125Giant/nestswing125Giant.jpg";
+ 
+       // titleImg : Люлка за щъркелово гнездо 125 см - цветна (с мрежа) ГИГАНТ
+       storkNestSwing125GiantUrl = "assets/imgswings/storkNestSwing125Giant/storkNestSwing125Giant.jpg";
+ 
+       // titleImg : Люлка щъркелово гнездо 60 см - цветна (без мрежа)
+       storkNestSwingCoWOutNetUrl="assets/imgswings/storkNestSwing60CoWOutNet/storkNestSwingCoWOutNet.jpg";
+
+        // titleImg : Люлка щъркелово гнездо 60 см - цветна (с мрежа)
+       storkNestSwing60CoWithNetUrl = "assets/imgswings/storkNestSwing60CoWithNet/storkNestSwing60CoWithNet.jpg";
 
 }
